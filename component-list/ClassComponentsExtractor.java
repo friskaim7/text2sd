8b0bbd4f3cc31b12d3156b03ca982a3e731834a9
friskaim7 <friska.intan2001@gmail.com>
2023-08-14 23:00:09 +0700

    refactor to save the output to the corresponding output files

--- component-list/ClassComponentsExtractor.java
+++ component-list/ClassComponentsExtractor.java
@@ -4,25 +4,40 @@ import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.expr.AnnotationExpr;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.List;
 
 public class ClassComponentsExtractor {
     public static void main(String[] args) throws IOException {
-        File file = new File("E:/Work/Maucash/Playground/ReflectionExtractor/welab-application/welab-application-api/src/main/java/com/welab/application/common/AppUtil.java");
+        final String REPO_ROOT_PATH = "E:/Work/Maucash/Repos/21-repos/";
+        String inputFileName = "welab-application/welab-application-api/src/main/java/com/welab/application/common/AppUtil.java";
+        String outputFilename = "component-list/out/" + inputFileName.replace(".java", ".txt");;
+
+        File file = new File(REPO_ROOT_PATH + inputFileName);
         CompilationUnit cu = StaticJavaParser.parse(file);
 
-        List<MethodDeclaration> methods = cu.findAll(MethodDeclaration.class);
-        for (MethodDeclaration method : methods) {
-            System.out.println("Method Name: " + method.getNameAsString());
+        // Create parent directories for the output file
+        File outputFile = new File(outputFilename);
+        outputFile.getParentFile().mkdirs();
 
-            // Mengambil annotation yang digunakan dalam method
-            List<AnnotationExpr> annotations = method.getAnnotations();
-            for (AnnotationExpr annotation : annotations) {
-                System.out.println("Annotation: " + annotation);
+        try (PrintStream printStream = new PrintStream(new FileOutputStream(outputFilename))) {
+            System.setOut(printStream);
+
+            List<MethodDeclaration> methods = cu.findAll(MethodDeclaration.class);
+            for (MethodDeclaration method : methods) {
+                System.out.println("Method Name: " + method.getNameAsString());
+
+                List<AnnotationExpr> annotations = method.getAnnotations();
+                for (AnnotationExpr annotation : annotations) {
+                    System.out.println("Annotation: " + annotation);
+                }
+
+                System.out.println("-----------------------");
             }
-
-            System.out.println("-----------------------");
         }
+
+        System.out.println("Output has been written to " + outputFilename);
     }
 }
