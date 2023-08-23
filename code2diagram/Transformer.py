6eb9198945d8925af3cef08916e937721c2ce745
friskaim7 <friska.intan2001@gmail.com>
2023-08-23 17:33:38 +0700

    update Transformer to write method informations

--- code2diagram/Transformer.py
+++ code2diagram/Transformer.py
@@ -19,13 +19,25 @@ def determine_static_mod(var_static_mod):
         static_string = "{static}"
     return static_string
 
-def class_var_to_write(class_var):
-    c_var_access_mod = determine_access_mod(class_var["class_var_access_mod"])
-    static_string = determine_static_mod(class_var["class_static_mod"])
-    c_var_type = class_var["class_var_type"]
-    c_var_name = class_var["class_var_name"]    
-                        
-    return f"{class_name} : {c_var_access_mod} {static_string} {c_var_type} {c_var_name}\n"
+def class_member_to_string(class_name, class_member):
+    access_mod = determine_access_mod(class_member["access_mod"])
+    static_string = determine_static_mod(class_member["static_mod"])
+    name = class_member["name"]
+
+    if "return_type" in class_member:
+        return_type = class_member["return_type"]
+        param_string = ""
+        params = class_member["params"]
+        print(params)
+        for param in params:
+            param_name = param["name"]
+            param_type = param["type"]
+            param_string += f"{param_name}: {param_type},"
+        param_string = param_string.rstrip(",")  # Remove trailing commas
+        return f"{class_name} : {access_mod} {static_string} {return_type} {name}({param_string})\n"
+    else:
+        var_type = class_member["type"]
+        return f"{class_name} : {access_mod} {static_string} {var_type} {name}\n"
 
 with open(input_filepath) as input_file:
     data = json.load(input_file)
@@ -40,16 +52,18 @@ with open(input_filepath) as input_file:
             # iterate each class
             for class_info in classes:
                 # Write class name to the text file
-                class_name = class_info.get("class_name", "")
+                class_name = class_info.get("name", "")
                 output_file.write(f"class {class_name}\n")
 
                 # Write class fields/variables
-                class_vars = class_info.get("class_vars", [])
+                class_vars = class_info.get("vars", [])
                 for class_var in class_vars:
-                    output_file.write(class_var_to_write(class_var))
+                    output_file.write(class_member_to_string(class_name, class_var))
                 
                 # Write class methods
                 methods = class_info.get("methods", [])
+                for method in methods:
+                    output_file.write(class_member_to_string(class_name, method))
 
         output_file.write("@enduml\n")
 
