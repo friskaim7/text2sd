d5c5184e083f0a5213b0fe161389c59060a209e1
friskaim7 <friska.intan2001@gmail.com>
2023-08-23 14:49:05 +0700

    update Transformer to write class names and fields

--- code2diagram/Transformer.py
+++ code2diagram/Transformer.py
@@ -2,19 +2,50 @@ import json
 from pprint import pprint
 
 input_filepath = "code2diagram/resource/input.json"
-output_filepath = "code2diagram/out/class_diagram.txt"
+output_filepath = "code2diagram/out/class_diagram.puml"
 
-with open(input_filepath) as json_file:
-    data = json.load(json_file)
-    for package_data in data:
-        package_name = package_data.get("package_name", "")
-        classes = package_data.get("classes", [])
-        
-        for class_info in classes:
-            class_name = class_info.get("class_name", "")
-            
-            # Write class name to the text file for each package
-            with open(output_filepath, 'a') as txt_file:
-                txt_file.write(class_name + '\n')
-        
-        print(f"Class names from '{input_filepath}' have been saved to {output_filepath}.")
+def determine_access_mod(access_mod):
+    access_modifiers = {
+        "public": "+",
+        "protected": "#",
+        "default": "~",
+        "private": "-"
+    }
+    return access_modifiers[access_mod]
+
+with open(input_filepath) as input_file:
+    data = json.load(input_file)
+    with open(output_filepath, 'a') as output_file:
+        output_file.write("@startuml\n")
+
+        # iterate each file data
+        for package_data in data:
+            package_name = package_data.get("package_name", "")
+            classes = package_data.get("classes", [])
+
+            # iterate each class
+            for class_info in classes:
+                # Write class name to the text file
+                class_name = class_info.get("class_name", "")
+                output_file.write(f"class {class_name}\n")
+
+                # Write class fields/variables
+                class_vars = class_info.get("class_vars", [])
+                for class_var in class_vars:
+                    c_var_access_mod = determine_access_mod(class_var["class_var_access_mod"])
+                    c_var_type = class_var["class_var_type"]
+                    c_var_name = class_var["class_var_name"]
+
+                    # determine class static modifier
+                    static_string = ""
+                    if class_var["class_static_mod"]:
+                        static_string = "{static}"
+                                        
+                    output_file.write(f"{class_name} : {c_var_access_mod} {static_string} {c_var_type} {c_var_name}\n")
+                
+                # Write class methods
+                methods = class_info.get("methods", [])
+
+        output_file.write("@enduml\n")
+
+print(f"Class names from '{input_filepath}' have been saved to {output_filepath}.")
