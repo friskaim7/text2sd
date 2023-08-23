98f0d37f16735cdabd09940102b9c1e550db04f0
friskaim7 <friska.intan2001@gmail.com>
2023-08-23 11:31:24 +0700

    update Transformer to the new JSON input format

--- code2diagram/Transformer.py
+++ code2diagram/Transformer.py
@@ -1,46 +1,20 @@
 import json
 from pprint import pprint
 
-input_file_name = "code2diagram/resource/input.json"
+input_filepath = "code2diagram/resource/input.json"
+output_filepath = "code2diagram/out/class_diagram.txt"
 
-with open(input_file_name) as json_file:
+with open(input_filepath) as json_file:
     data = json.load(json_file)
-    # pprint(data)
-    # for filedata in data:
-    #     filedata[]
-
     for package_data in data:
-        file_relative_path = package_data.get("file_relative_path", "")
+        package_name = package_data.get("package_name", "")
+        classes = package_data.get("classes", [])
         
-        for package_name, classes in package_data.items():
-            print("package_name: " + package_name)
-            print("classes: " + classes)
-            if package_name != "file_relative_path":
-                for class_info in classes:
-                    for class_name, class_data in class_info.items():
-                        print("File Relative Path:", file_relative_path)
-                        print("Package Name:", package_name)
-                        print("Class Name:", class_name)
-                        
-                        # class_variables = class_data.get("class_variables", [])
-                        # methods = class_data.get("methods", [])
-                        
-                        # for variable in class_variables:
-                        #     variable_name = variable.get("class_variable_name", "")
-                        #     variable_modifier = variable.get("class_variable_modifier", "")
-                        #     print("Class Variable:", variable_modifier, variable_name)
-                        
-                        # for method in methods:
-                        #     for method_name, method_info in method.items():
-                        #         method_variables = method_info.get("method_variables", [])
-                        #         calling_methods = method_info.get("calling", [])
-                        #         print("Method:", method_name)
-                                
-                        #         for m_variable in method_variables:
-                        #             m_variable_name = m_variable.get("method_variable_name", "")
-                        #             m_variable_modifier = m_variable.get("method_variable_modifier", "")
-                        #             print("Method Variable:", m_variable_modifier, m_variable_name)
-                                
-                        #         print("Calling Methods:", calling_methods)
-                        
-                        # print()
+        for class_info in classes:
+            class_name = class_info.get("class_name", "")
+            
+            # Write class name to the text file for each package
+            with open(output_filepath, 'a') as txt_file:
+                txt_file.write(class_name + '\n')
+        
+        print(f"Class names from '{input_filepath}' have been saved to {output_filepath}.")
