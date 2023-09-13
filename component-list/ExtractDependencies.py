4004019b8eadb294e64e676000b591952407b4a8
friskaim7 <friska.intan2001@gmail.com>
2023-09-13 15:31:58 +0700

    fix not printed Depends Upon issues

--- component-list/ExtractDependencies.py
+++ component-list/ExtractDependencies.py
@@ -1,10 +1,20 @@
-def get_repo_name(package_name):
+ROOT_PACKAGE_NAME = "com.welab."
+
+def get_repo_name(package_name) -> str:
     splitted_package_name = package_name.split('.')
-    return '.'.join(splitted_package_name[:3])
+    return '.'.join(splitted_package_name[:3]).strip()
 
 def get_package_name(package_line):
     splitted_package_line = package_line.split(' ')
-    return splitted_package_line[-1]
+    return splitted_package_line[-1].strip()
+
+def get_dependency_list(file, dep_type="depend upon", stoper="Used By:"):
+    print(dep_type)
+    line = file.readline()
+    while stoper not in line:
+        if ROOT_PACKAGE_NAME in line:
+            print(f'{dep_type} {line}')  # Print each dependency
+        line = file.readline()
 
 if __name__ == "__main__":
     input_filename = "./component-list/out/JDepend/output - Copy.txt"
@@ -14,31 +24,20 @@ if __name__ == "__main__":
         # Iterate as long as EOF has not been reached
         while line:
             # Change Package and Repo name when enter new package
-            if (line.startswith("- Package: ")) and ("com.welab." in line):
+            # Non Welab packages are ignored
+            if (line.startswith("- Package: ")) and (ROOT_PACKAGE_NAME in line):
                 package_name = get_package_name(line)
                 repo_name = get_repo_name(package_name)
-                print(f'package_name: {package_name}')
-                print(f'repo_name: {repo_name}')
+                print()
+                print(f"package_name: {package_name}")
+                print(f"repo_name: {repo_name}")
             
             # Get next line
             line = file.readline()
 
             # Iterate next line as long as there is "com.welab" after "Depends Upon"
             if "Depends Upon:" in line:
-                while line:
-                    line = file.readline()
-                    if "com.welab." in line:
-                        print(f'depend upon line: {line}')
-                        line = file.readline()
-                        # depend_upon_list[f"{package_name}"]
-                    elif "--------------------------------------------------" in line:
-                        break
-            if "Used By:" in line:
-                while line:
-                    line = file.readline()
-                    if "com.welab." in line:
-                        print(f'used by line: {line}')
-                        line = file.readline()
-                        # depend_upon_list[f"{package_name}"]
-                    elif "--------------------------------------------------" in line:
-                        break
+                get_dependency_list(file, "Depends Upon:", "Used By:")
+            # if "Used By:" in line:
+            elif "Used By:" in line:
+                get_dependency_list(file, "Used By:", "--------------------------------------------------")
