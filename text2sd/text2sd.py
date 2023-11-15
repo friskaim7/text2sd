8a9d27c54e04262667b10128ce1eabfe077b0c53
friskaim7 <friska.intan2001@gmail.com>
2023-11-15 15:50:45 +0700

    refactoring and documenting the methods

--- text2sd/text2sd.py
+++ text2sd/text2sd.py
@@ -1,9 +1,10 @@
-""" This file containing the function needed to convert .txt file into .puml """
+""" This file containing the functions needed to convert .txt file into .puml """
 
 import re
 from nonbinary_tree import build_tree_from_file
 
 INPUT_FILENAME = "./text2sd/sample/[Input] Sample.txt"
+OUTPUT_FILENAME = "./text2sd/output/sample.puml"
 CLASS = "cls"
 METHOD = "method"
 RETURN = "ret"
@@ -13,18 +14,15 @@ def count_ltab(line):
     return line.lstrip().count('\t')
 
 def has_colon(line):
+    """Comfirm if the given line has any number of colon in any position"""
     return ':' in line
 
 def has_return(line):
+    """Comfirm if the given line has any number of ">>" in any position"""
     return ">>" in line
 
-def property_identifier2(line):
-    pattern = r"\s*(?P<cls>\w+)\.(?P<method>[\w(), ]+)\s>>\s(?P<ret>[\w<>]+):?"
-    matches = re.match(pattern, line)
-    return matches.groupdict() if matches else None
-
 def property_identifier(line):
-    """	return class, method, and return """
+    """	Return class, method, and return """
     line_prop = {CLASS: "", METHOD: "", RETURN: ""}
     if line is None:
         return line_prop
@@ -35,52 +33,43 @@ def property_identifier(line):
     line_prop[RETURN] = method_return_raw[1][:-1].strip() if method_return_raw[1].endswith(":") else method_return_raw[1].strip()
     return line_prop
 
-
-def formatted_caller(caller_node, called_node):
-    caller_line, called_line = formatting_passer(caller_node,called_node)
-    caller_prop = property_identifier(caller_line)
-    called_prop = property_identifier(called_line)
-    return f"{caller_prop[CLASS]} -> {called_prop[CLASS]}: {called_prop[METHOD]}"
-
-def formatted_return(caller_node, called_node):
-    caller_line, called_line = formatting_passer(caller_node,called_node)
-    caller_prop = property_identifier(caller_line)
-    called_prop = property_identifier(called_line)
-    return f"{caller_prop[CLASS]} <-- {called_prop[CLASS]}: {called_prop[RETURN]}"
-
-def print_tree(node, indent=0):
-    if node is not None:
-        print("  " * indent + node.data)
-        for child in node.children:
-            print_tree(child, indent + 1)
-
 def formatting_passer(parent, child):
+    """ A helper function for parentless nodes"""
     if parent is None:
         return None, child.data
-    else:
-        return parent.data, child.data
+    return parent.data, child.data
 
-def print_custom(node, parent=None):
-    if node is not None:
-        print(formatted_caller(parent, node))
-        for child in node.children:
-            print_custom(child, node)
-        print(formatted_return(parent, node))
+def get_output_props(caller_node, called_node):
+    """Get line properties for both caller and called method """
+    caller_line, called_line = formatting_passer(caller_node,called_node)
+    caller_prop = property_identifier(caller_line)
+    called_prop = property_identifier(called_line)
+    return caller_prop, called_prop
+
+def get_formatted_caller(caller_node, called_node):
+    """ Format inputed caller and called lines into PlantUML sequence diagram call syntax"""
+    caller_prop, called_prop = get_output_props(caller_node, called_node)
+    return f"{caller_prop[CLASS]} -> {called_prop[CLASS]}: {called_prop[METHOD]}"
+
+def get_formatted_return(caller_node, called_node):
+    """ Format inputed caller and called lines into PlantUML sequence diagram return syntax"""
+    caller_prop, called_prop = get_output_props(caller_node, called_node)
+    return f"{caller_prop[CLASS]} <-- {called_prop[CLASS]}: {called_prop[RETURN]}"
 
 def save_to_puml(puml_file, node, parent=None):
+    """" Save inputed tree into a puml file with PlantUML sequence diagram syntax"""
     if node is not None:
-        print(formatted_caller(parent, node))
-        puml_file.write(formatted_caller(parent, node) + '\n')
+        puml_file.write(get_formatted_caller(parent, node) + '\n')
         for child in node.children:
             save_to_puml(puml_file, child, node)
-        print(formatted_return(parent, node))
-        puml_file.write(formatted_return(parent, node) + '\n')
+        puml_file.write(get_formatted_return(parent, node) + '\n')
+
 
 def main():
     """ Run the program """
     with open(INPUT_FILENAME, 'r', encoding='utf-8') as input_file:
         tree_root = build_tree_from_file(input_file)
-        with open("./text2sd/output/sample.puml", "w", encoding='utf-8') as puml_file:
+        with open(OUTPUT_FILENAME, "w", encoding='utf-8') as puml_file:
             save_to_puml(puml_file, tree_root)
 
 if __name__ == "__main__":
